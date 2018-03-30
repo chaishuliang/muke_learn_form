349ecf5da851ed7f0f470d579de35c98783d37e3
chaishuliang <chiashuliang@126.com>
2018-03-30 09:53:08 +0800

    entrust

--- resources/views/student/index.blade.php
+++ resources/views/student/index.blade.php
@@ -13,21 +13,6 @@
                 </div>
                 <button type="submit" class="btn btn-default">查询</button>
             </form>
-            @role('admin')
-            <p>This is visible to users with the admin role. Gets translated to
-                \Entrust::role('admin')</p>
-            @endrole
-
-            @permission('create-post')
-            <p>This is visible to users with the given permissions. Gets translated to
-                \Entrust::can('manage-admins'). The @ can directive is already taken by core
-                laravel authorization package, hence the @ permission directive instead.</p>
-            @endpermission
-
-            @ability('admin,owner','create-post,edit-user')
-            <p>This is visible to users with the given abilities. Gets translated to
-                \Entrust::ability('admin,owner', 'create-post,edit-user')</p>
-            @endability
         </div>
     </div>
     <!-- 自定义内容区域 -->

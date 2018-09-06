38fce8ad9665e9f580f66b017612cac6b089a191
chaishuliang <chaishuliang@126.com>
2018-09-06 21:32:20 +0800

    model

--- app/Student.php
+++ app/Student.php
@@ -8,15 +8,40 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Student extends Model
 {
 
+    use SoftDeletes;
+
     const SEX_UN = 10;  //未知
     const SEX_BOY = 20; // 男
     const SEX_GRIL = 30;    //女
 
     protected $table = 'student';
 
-    protected $fillable = ['name', 'age', 'sex']; //模型中定义哪些属性是可以进行赋值的
+    //设置主键
+    public $primaryKey = 'id';
 
-    //public $timestamps = true; //默认为true
+    //赋值白名单
+    //protected $fillable = ['name', 'age', 'sex']; //模型中定义哪些属性是可以进行赋值的
+
+    /**
+     * 默认情况下，Eloquent 会预计你的数据表中有 created_at 和 updated_at 字段。
+     * 如果不希望让 Eloquent 来自动维护这两个字段，可在模型内将 $timestamps 属性设置为 false
+     * @var bool
+     */
+    public $timestamps = true; //默认为true
+
+    //设置日期时间格式
+    public $dateFormat = '';//根据数据库自动获取
+    //public $dateFormat = 'U';//时间戳
+
+    //赋值黑名单
+    protected $guarded = ['id'];
+
+    /**
+     * 应该被调整为日期的属性.
+     *
+     * @var array
+     */
+    protected $dates = ['deleted_at'];
 
     //提供一个定制的时间戳格式
     /*protected function getDateFormat()
